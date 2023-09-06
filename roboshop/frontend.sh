267e4e022861a4de6381615dfd7c267e9da4e5a3
unknown <maheshhr88505@gmail.com>
2023-09-06 13:38:08 +0530

    Auto Push

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -2,27 +2,43 @@
 
 COMPONENT=frontend
 
+
+
 ID=$(id -u)
 if [ $ID -ne 0 ] ; then
     echo -e "\e[31m This script is expected to be run as a root user or with sudo previlege \e[0m"
     exit 1
 fi
 
+
+stat(){
+
+    if [ $? -eq 0 ] ; then
+            echo -e "\e[35m Success \e[0m"
+        else
+            echo -e "\e[35m Failure \e[0m"
+            exit 2
+    fi
+}
+
 echo -n "Install the nginx: "
 yum install nginx -y &>> "/tmp/$COMPONENT.log"
-if [ $? -eq 0 ] ; then
-        echo -e "\e[35m Success \e[0m"
-    else
-        echo -e "\e[35m Failure \e[0m"
-fi
+stat $?
 
 echo -n "Download the $COMPONENT component"
 curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
-if [ $? -eq 0 ] ; then
-        echo -e "\e[35m Success \e[0m"
-    else
-        echo -e "\e[35m Failure \e[0m"
-fi
+stat $?
+
+echo -n "Performing Cleanup"
+cd /usr/share/nginx/html
+rm -rf *
+stat $?
+
+# unzip /tmp/frontend.zip
+# mv frontend-main/* .
+# mv static/* .
+# rm -rf frontend-main README.md
+# mv localhost.conf /etc/nginx/default.d/roboshop.conf
 
 # systemctl enable nginx
 # systemctl start nginx
