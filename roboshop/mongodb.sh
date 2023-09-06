8b08984049b4d5950af2631882877577bba3f4bf
unknown <maheshhr88505@gmail.com>
2023-09-06 16:06:03 +0530

    Auto Push

--- roboshop/mongodb.sh
+++ roboshop/mongodb.sh
@@ -34,5 +34,20 @@ echo -n "Update the $COMPONENT IP adress :"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 stat $?
 echo -n "Stating the $COMPONENT service"
+systemctl daemon-reload &>> LOGFILE
+systemctl enable mongod &>> LOGFILE
 systemctl start mongod &>> LOGFILE
+stat $?
+
+echo -n "Download the schema of $COMPONENT :"
+curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
+stat $?
+echo -n "Extracting the $COMPONENT schema :"
+cd /tmp
+unzip $COMPONENT.zip
+stat $?
+echo -n "Injecting the schema"
+cd $COMPONENT-main
+mongo < catalogue.js &>>LOGFILE
+mongo < users.js &>>LOGFILE
 stat $?
