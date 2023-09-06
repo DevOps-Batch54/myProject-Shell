7d0744fb196cc97c6f91ccb740a0038b337fefd4
unknown <maheshhr88505@gmail.com>
2023-09-06 15:44:51 +0530

    Auto Push

--- roboshop/mongodb.sh
+++ roboshop/mongodb.sh
@@ -4,7 +4,7 @@ COMPONENT=mongodb
 ID=$(id -u)
 if [ $ID -ne 0 ] ; then
         echo -e "\e[35m This script is expected to be run as a root user or with a sudo previlege user \e[0m"
-        exit1
+        exit 1
 fi
 
 
@@ -13,7 +13,7 @@ stat(){
         echo -e "\e[32m Success \e[0m"
     else
         echo -e "\e[32m Failure \e[0m"
-        exit2
+        exit 2
 }
 
 echo -n "Download $COMPONENT component"
