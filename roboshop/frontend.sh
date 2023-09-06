6aa6fe6261e0a428449990c04e2461d02185be39
unknown <maheshhr88505@gmail.com>
2023-09-06 12:50:35 +0530

    Auto Push

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -1,5 +1,6 @@
 #!bin/bash
 
+COMPONENT=frontend
 ID=$(id -u)
 if [ $ID -ne 0 ] ; then
     echo -e "\e[31m This script is expected to be run as a root user or with sudo previlege \e[0m"
@@ -7,7 +8,12 @@ if [ $ID -ne 0 ] ; then
 fi
 
 echo -n "Install the nginx: "
-yum install nginx -y &>> /tmp/frontend.log
+yum install nginx -y &>> "/tmp/$COMPONENT.log"
+if [ $? -eq 0 ] ; then
+        echo -e "\e[35m Success \e[0m"
+    elce
+        echo -e "\e[35m Failure \e[0m"
+fi
 
 
 # yum install nginx -y
