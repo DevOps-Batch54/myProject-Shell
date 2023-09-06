f060a26c2b2379ba0025262ba0776d386fd5d8b2
unknown <maheshhr88505@gmail.com>
2023-09-06 12:58:49 +0530

    Auto Push

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -1,6 +1,7 @@
 #!bin/bash
 
 COMPONENT=frontend
+
 ID=$(id -u)
 if [ $ID -ne 0 ] ; then
     echo -e "\e[31m This script is expected to be run as a root user or with sudo previlege \e[0m"
@@ -15,7 +16,13 @@ if [ $? -eq 0 ] ; then
         echo -e "\e[35m Failure \e[0m"
 fi
 
+echo -n "Download the $COMPONENT component"
+curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
+if [ $? -eq 0 ] ; then
+        echo -e "\e[35m Success \e[0m"
+    else
+        echo -e "\e[35m Failure \e[0m"
+fi
 
-# yum install nginx -y
 # systemctl enable nginx
 # systemctl start nginx
