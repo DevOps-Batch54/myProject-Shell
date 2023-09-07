fd4996c83018b24c0e256c89f1f5874f0bfa6995
unknown <maheshhr88505@gmail.com>
2023-09-07 14:23:45 +0530

    Auto Push

--- roboshop/mongodb.sh
+++ roboshop/mongodb.sh
@@ -1,22 +1,7 @@
 #!bin/bash
 
 COMPONENT=mongodb
-LOGFILE="/tmp/$COMPONENT.log"
-ID=$(id -u)
-if [ $ID -ne 0 ] ; then
-        echo -e "\e[35m This script is expected to be run as a root user or with a sudo previlege user \e[0m"
-        exit 1
-fi
-
-
-stat(){
-    if [ $? -eq 0 ] ; then
-        echo -e "\e[32m Success \e[0m"
-    else
-        echo -e "\e[32m Failure \e[0m"
-        exit 2
-    fi    
-}
+source common.sh
 
 echo -n "Download $COMPONENT component"
 curl -s -o /etc/yum.repos.d/$COMPONENT.repo https://raw.githubusercontent.com/stans-robot-project/$COMPONENT/main/mongo.repo
