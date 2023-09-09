af5fcf81adb207d21fdd509319f0cf6d7653de1c
unknown <maheshhr88505@gmail.com>
2023-09-09 16:06:46 +0530

    Auto Push

--- roboshop/mysql.sh
+++ roboshop/mysql.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 COMPONENT=mysql
-source=common.sh
+source common.sh
 echo -n "Configure the $COMPONENT repo :"
-curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/stans-robot-project/mysql/main/mysql.repo
+curl -s -L -o /etc/yum.repos.d/$COMPONENT.repo https://raw.githubusercontent.com/stans-robot-project/$COMPONENT/main/$COMPONENT.repo
 stat $?
 
 echo -n "Installing $COMPONENT :"
-yum install mysql-community-server -y &>> LOGFILE
+yum install $COMPONENT-community-server -y &>> LOGFILE
 stat $?
 echo -n "Starting the $COMPONENT"
 systemctl enable mysqld &>> LOGFILE
