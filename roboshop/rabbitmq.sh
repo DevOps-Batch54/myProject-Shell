2a4eb256754876b395a6c62c7f090f4f0ba70a78
unknown <maheshhr88505@gmail.com>
2023-09-11 16:38:50 +0530

    Auto Push

---
+++ roboshop/rabbitmq.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+COMOPONENT=rabbitmq
+source common.sh
+
+echo -e " ******* \e[35m $COMPONENT installation has started \e[0m *******"
+echo -n "Configuration the $COMPONENT repo"
+curl -s https://packagecloud.io/install/repositories/$COMOPONENT/erlang/script.rpm.sh | sudo bash
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash
+stat $?
+
+echo -n "Installing $COMPONENT :"
+yum install rabbitmq-server -y &>> $LOGFILE
