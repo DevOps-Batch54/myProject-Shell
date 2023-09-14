546592dc864304bab2fb70bd14cd2f43eddf25b9
unknown <maheshhr88505@gmail.com>
2023-09-14 18:09:13 +0530

    Auto Push

--- roboshop/create.sh
+++ roboshop/create.sh
@@ -2,6 +2,7 @@
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7-Backup" | jq '.Images[].ImageId' | sed -e 's/"//g')
 SG_ID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=B54-Allow-All | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')
 echo -e "AMI id used to launch the EC2 is \e[32m $AMI_ID \e[0m"
+echo -e "AMI id used to launch the EC2 is \e[35m $SG_ID \e[0m"
 #aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7-Backup"   #aws cli commands for ami
 
 echo -e "******* Launching Server *******"
