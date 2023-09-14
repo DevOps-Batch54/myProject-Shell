1a22ee02e53a8f282d01c55768427e34d0d23620
unknown <maheshhr88505@gmail.com>
2023-09-14 17:42:26 +0530

    Auto Push

--- roboshop/create.sh
+++ roboshop/create.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7-Backup" | jq '.Images[].ImageId')
+AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7-Backup" | jq '.Images[].ImageId' | sed -e 's/"//g')
 
 echo "AMI id is $AMI_ID"
 #aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7-Backup"   #aws cli commands for ami
